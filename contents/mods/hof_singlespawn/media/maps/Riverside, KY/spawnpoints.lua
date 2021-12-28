6d287684fb41d903a9f4e8b8723549c065c7071c
Grant <sweatgrant@gmail.com>
2021-12-28 04:46:20 -0600

    feat(Spawn Points For All Occupations?) Untested.

---
+++ contents/mods/hof_singlespawn/media/maps/Riverside, KY/spawnpoints.lua
@@ -0,0 +1,200 @@
+function SpawnPoints() 
+    return {
+      fireofficer = {
+        { worldX = 20, worldY = 17, posX = 57, posY = 249, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 266, posY = 228, posZ = 0 },
+        { worldX = 21, worldY = 18, posX = 51, posY = 20, posZ = 0 },
+        { worldX = 20, worldY = 18, posX = 282, posY = 49, posZ = 0 },
+        { worldX = 21, worldY = 18, posX = 219, posY = 79, posZ = 0 },
+        { worldX = 21, worldY = 18, posX = 227, posY = 31, posZ = 0 },
+        { worldX = 22, worldY = 17, posX = 115, posY = 231, posZ = 0 }
+      },
+      policeofficer = {
+        { worldX = 21, worldY = 18, posX = 219, posY = 79, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 57, posY = 249, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 266, posY = 228, posZ = 0 },
+        { worldX = 19, worldY = 17, posX = 114, posY = 216, posZ = 0 },
+        { worldX = 19, worldY = 17, posX = 79, posY = 258, posZ = 0 },
+        { worldX = 21, worldY = 18, posX = 51, posY = 20, posZ = 0 },
+        { worldX = 20, worldY = 18, posX = 284, posY = 106, posZ = 0 },
+        { worldX = 20, worldY = 18, posX = 282, posY = 49, posZ = 0 },
+        { worldX = 21, worldY = 18, posX = 219, posY = 79, posZ = 0 },
+        { worldX = 21, worldY = 18, posX = 227, posY = 31, posZ = 0 },
+        { worldX = 22, worldY = 17, posX = 205, posY = 270, posZ = 0 },
+        { worldX = 21, worldY = 17, posX = 71, posY = 168, posZ = 0 }
+      },
+      parkranger = {
+        { worldX = 20, worldY = 17, posX = 57, posY = 249, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 266, posY = 228, posZ = 0 },
+        { worldX = 21, worldY = 18, posX = 51, posY = 20, posZ = 0 },
+        { worldX = 20, worldY = 18, posX = 282, posY = 49, posZ = 0 },
+        { worldX = 21, worldY = 18, posX = 219, posY = 79, posZ = 0 },
+        { worldX = 21, worldY = 18, posX = 227, posY = 31, posZ = 0 },
+        { worldX = 22, worldY = 17, posX = 115, posY = 231, posZ = 0 }
+      },
+      constructionworker = {
+        { worldX = 21, worldY = 17, posX = 71, posY = 168, posZ = 0 },
+        { worldX = 17, worldY = 20, posX = 295, posY = 56, posZ = 0 },
+        { worldX = 17, worldY = 20, posX = 233, posY = 63, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 57, posY = 249, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 266, posY = 228, posZ = 0 },
+        { worldX = 21, worldY = 18, posX = 51, posY = 20, posZ = 0 },
+        { worldX = 22, worldY = 17, posX = 136, posY = 143, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 71, posY = 223, posZ = 0 },
+        { worldX = 19, worldY = 18, posX = 260, posY = 23, posZ = 0 }
+      },
+      securityguard = {
+        { worldX = 17, worldY = 20, posX = 295, posY = 56, posZ = 0 },
+        { worldX = 17, worldY = 20, posX = 233, posY = 63, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 57, posY = 249, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 266, posY = 228, posZ = 0 },
+        { worldX = 19, worldY = 17, posX = 114, posY = 216, posZ = 0 },
+        { worldX = 19, worldY = 17, posX = 79, posY = 258, posZ = 0 },
+        { worldX = 21, worldY = 18, posX = 51, posY = 20, posZ = 0 },
+        { worldX = 19, worldY = 18, posX = 260, posY = 23, posZ = 0 },
+        { worldX = 20, worldY = 18, posX = 284, posY = 106, posZ = 0 },
+        { worldX = 20, worldY = 18, posX = 282, posY = 49, posZ = 0 }
+      },
+      carpenter = {
+        { worldX = 21, worldY = 17, posX = 71, posY = 168, posZ = 0 },
+        { worldX = 17, worldY = 20, posX = 295, posY = 56, posZ = 0 },
+        { worldX = 17, worldY = 20, posX = 233, posY = 63, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 57, posY = 249, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 266, posY = 228, posZ = 0 },
+        { worldX = 21, worldY = 18, posX = 51, posY = 20, posZ = 0 },
+        { worldX = 22, worldY = 17, posX = 136, posY = 143, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 71, posY = 223, posZ = 0 },
+        { worldX = 19, worldY = 18, posX = 260, posY = 23, posZ = 0 }
+      },
+      burglar = {
+        { worldX = 17, worldY = 20, posX = 295, posY = 56, posZ = 0 },
+        { worldX = 17, worldY = 20, posX = 233, posY = 63, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 57, posY = 249, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 266, posY = 228, posZ = 0 },
+        { worldX = 19, worldY = 17, posX = 114, posY = 216, posZ = 0 },
+        { worldX = 19, worldY = 17, posX = 79, posY = 258, posZ = 0 },
+        { worldX = 21, worldY = 18, posX = 51, posY = 20, posZ = 0 },
+        { worldX = 19, worldY = 18, posX = 260, posY = 23, posZ = 0 }
+      }
+      chef = {
+        { worldX = 21, worldY = 18, posX = 227, posY = 31, posZ = 0 },
+        { worldX = 22, worldY = 17, posX = 115, posY = 231, posZ = 0 },
+        { worldX = 22, worldY = 17, posX = 205, posY = 270, posZ = 0 },
+        { worldX = 22, worldY = 18, posX = 159, posY = 87, posZ = 0 },
+        { worldX = 20, worldY = 21, posX = 179, posY = 73, posZ = 0 },
+        { worldX = 21, worldY = 17, posX = 71, posY = 168, posZ = 0 }
+      },
+      repairman = {
+        { worldX = 17, worldY = 20, posX = 295, posY = 56, posZ = 0 },
+        { worldX = 17, worldY = 20, posX = 233, posY = 63, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 57, posY = 249, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 266, posY = 228, posZ = 0 },
+        { worldX = 19, worldY = 17, posX = 114, posY = 216, posZ = 0 },
+        { worldX = 19, worldY = 17, posX = 79, posY = 258, posZ = 0 },
+        { worldX = 21, worldY = 18, posX = 51, posY = 20, posZ = 0 },
+        { worldX = 19, worldY = 18, posX = 260, posY = 23, posZ = 0 },
+        { worldX = 22, worldY = 17, posX = 136, posY = 143, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 71, posY = 223, posZ = 0 }
+      },
+      farmer = {
+        { worldX = 18, worldY = 19, posX = 283, posY = 66, posZ = 0 },
+        { worldX = 18, worldY = 18, posX = 44, posY = 152, posZ = 0 },
+        { worldX = 18, worldY = 18, posX = 10, posY = 46, posZ = 0 },
+        { worldX = 19, worldY = 19, posX = 63, posY = 34, posZ = 0 },
+        { worldX = 19, worldY = 18, posX = 93, posY = 276, posZ = 0 },
+        { worldX = 19, worldY = 18, posX = 161, posY = 125, posZ = 0 },
+      },
+      fisherman = {
+        { worldX = 17, worldY = 20, posX = 295, posY = 56, posZ = 0 },
+        { worldX = 17, worldY = 20, posX = 233, posY = 63, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 57, posY = 249, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 266, posY = 228, posZ = 0 },
+        { worldX = 19, worldY = 17, posX = 114, posY = 216, posZ = 0 },
+        { worldX = 19, worldY = 17, posX = 79, posY = 258, posZ = 0 },
+        { worldX = 21, worldY = 18, posX = 51, posY = 20, posZ = 0 },
+        { worldX = 19, worldY = 18, posX = 260, posY = 23, posZ = 0 }
+      },
+      doctor = {
+        { worldX = 21, worldY = 18, posX = 227, posY = 31, posZ = 0 },
+        { worldX = 22, worldY = 17, posX = 115, posY = 231, posZ = 0 },
+        { worldX = 22, worldY = 17, posX = 205, posY = 270, posZ = 0 },
+        { worldX = 22, worldY = 18, posX = 159, posY = 87, posZ = 0 },
+        { worldX = 21, worldY = 17, posX = 172, posY = 171, posZ = 0 }
+      },
+      veteran = {
+        { worldX = 17, worldY = 20, posX = 295, posY = 56, posZ = 0 },
+        { worldX = 17, worldY = 20, posX = 233, posY = 63, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 57, posY = 249, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 266, posY = 228, posZ = 0 },
+        { worldX = 19, worldY = 17, posX = 114, posY = 216, posZ = 0 },
+        { worldX = 19, worldY = 17, posX = 79, posY = 258, posZ = 0 },
+        { worldX = 21, worldY = 18, posX = 51, posY = 20, posZ = 0 },
+        { worldX = 19, worldY = 18, posX = 260, posY = 23, posZ = 0 },
+        { worldX = 20, worldY = 18, posX = 284, posY = 106, posZ = 0 },
+        { worldX = 20, worldY = 18, posX = 282, posY = 49, posZ = 0 }
+      },
+      nurse = {
+        { worldX = 21, worldY = 18, posX = 227, posY = 31, posZ = 0 },
+        { worldX = 22, worldY = 17, posX = 115, posY = 231, posZ = 0 },
+        { worldX = 22, worldY = 17, posX = 205, posY = 270, posZ = 0 },
+        { worldX = 22, worldY = 18, posX = 159, posY = 87, posZ = 0 },
+        { worldX = 21, worldY = 17, posX = 172, posY = 171, posZ = 0 }
+      },
+      lumberjack = {
+        { worldX = 17, worldY = 20, posX = 295, posY = 56, posZ = 0 },
+        { worldX = 17, worldY = 20, posX = 233, posY = 63, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 57, posY = 249, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 266, posY = 228, posZ = 0 },
+        { worldX = 19, worldY = 17, posX = 114, posY = 216, posZ = 0 },
+        { worldX = 19, worldY = 17, posX = 79, posY = 258, posZ = 0 },
+        { worldX = 21, worldY = 18, posX = 51, posY = 20, posZ = 0 },
+        { worldX = 19, worldY = 18, posX = 260, posY = 23, posZ = 0 }
+      },
+      fitnessinstructor = {
+        { worldX = 20, worldY = 18, posX = 284, posY = 108, posZ = 0 },
+        { worldX = 20, worldY = 18, posX = 281, posY = 76, posZ = 0 },
+        { worldX = 20, worldY = 18, posX = 58, posY = 31, posZ = 0 },
+        { worldX = 22, worldY = 18, posX = 162, posY = 42, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 277, posY = 208, posZ = 0 },
+      },
+      burgerflipper = {
+        { worldX = 20, worldY = 17, posX = 131, posY = 212, posZ = 0 },
+        { worldX = 21, worldY = 17, posX = 171, posY = 117, posZ = 0 },
+        { worldX = 21, worldY = 17, posX = 292, posY = 126, posZ = 0 },
+        { worldX = 21, worldY = 17, posX = 195, posY = 241, posZ = 0 },
+        { worldX = 21, worldY = 17, posX = 102, posY = 211, posZ = 0 },
+      },
+      electrician = {
+        { worldX = 22, worldY = 18, posX = 162, posY = 42, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 277, posY = 208, posZ = 0 },
+        { worldX = 22, worldY = 17, posX = 136, posY = 143, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 71, posY = 223, posZ = 0 }
+        { worldX = 21, worldY = 18, posX = 67, posY = 184, posZ = 0 },
+      },
+      metalworker = {
+        { worldX = 19, worldY = 18, posX = 265, posY = 24, posZ = 0 },
+        { worldX = 21, worldY = 18, posX = 139, posY = 23, posZ = 0 },
+        { worldX = 21, worldY = 17, posX = 277, posY = 269, posZ = 0 },
+        { worldX = 22, worldY = 17, posX = 139, posY = 229, posZ = 0 }
+        { worldX = 22, worldY = 17, posX = 137, posY = 143, posZ = 0 },
+      },
+      mechanic = {
+        { worldX = 20, worldY = 17, posX = 76, posY = 247, posZ = 0 },
+        { worldX = 21, worldY = 18, posX = 34, posY = 87, posZ = 0 },
+        { worldX = 21, worldY = 18, posX = 205, posY = 144, posZ = 0 },
+        { worldX = 22, worldY = 18, posX = 57, posY = 122, posZ = 0 }
+        { worldX = 21, worldY = 17, posX = 40, posY = 158, posZ = 0 },
+      },
+      unemployed = {
+        { worldX = 17, worldY = 20, posX = 295, posY = 56, posZ = 0 },
+        { worldX = 17, worldY = 20, posX = 233, posY = 63, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 57, posY = 249, posZ = 0 },
+        { worldX = 20, worldY = 17, posX = 266, posY = 228, posZ = 0 },
+        { worldX = 19, worldY = 17, posX = 114, posY = 216, posZ = 0 },
+        { worldX = 19, worldY = 17, posX = 79, posY = 258, posZ = 0 },
+        { worldX = 21, worldY = 18, posX = 51, posY = 20, posZ = 0 },
+        { worldX = 19, worldY = 18, posX = 260, posY = 23, posZ = 0 }
+      }
+    }
+    end
+    
